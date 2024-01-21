724295aecd1031481a93319f216118e20185fc09
DenisPlotnikov <denisplotnikov7500@gmail.com>
2024-01-21 22:05:33 +0300

    первая фиксация

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('feedbackForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+   
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+   
+    // Здесь будет код для отправки данных на сервер
+   
+    alert('Сообщение отправлено!');
+    this.reset();
+  });
+
+
+  
